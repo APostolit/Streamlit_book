1464e96a8b16a4e770d80f4376d28e2746b0346a
APostolit <50382487+APostolit@users.noreply.github.com>
2025-11-18 09:54:12 +0000

    Update listing_2_5.py

--- pages/glava_2/listing_2_5.py
+++ pages/glava_2/listing_2_5.py
@@ -9,10 +9,10 @@ with open(pdf_file, "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
 # Создать объект для вывода в HTML
-pdf_display = (F'<embed src="data:application/pdf;base64,{base64_pdf}"'
-               F' width="1000" height="800" type="application/pdf">')
+pdf_d = f'<embed src="data:application/pdf;base64,{base64_pdf}"' \
+        f' width="1000" height="800" type="application/pdf">'
 
 st.subheader(":blue[Содержимое PDF файла с оглавлением]")
 
 # Отобразить содержимое PDF файла
-st.markdown(body=pdf_display, unsafe_allow_html=True)
+st.markdown(body=pdf_d, unsafe_allow_html=True)
